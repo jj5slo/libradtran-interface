15351b1efa8fc4de0f7a2b9989491e0b34f19f57
sano <s2210729@gl.cc.uec.ac.jp>
2025-10-27 18:05:52 +0900

    fitできていない

--- DaemonExec.sh
+++ DaemonExec.sh
@@ -5,6 +5,22 @@ pwd
 cd /lhome/sano2/SANO/research/estimate-profile/libradtran-interface
 # ./main 2022 6 2 3
 # ./main 2022 6 6 6 &> /tmp/libradtran-interface.log
+
+mv configs/pho3000 config.conf
+for month in 6; do
+# month=6
+#	for date in 1 21; do	
+	for date in 21; do
+#		for i in 5 10 15 20 25 30 35 40 45 50 65 70 75 80 85; do
+		for i in 5 15 35 45 75 85; do
+	 		echo "Progress: $month $date, $i"
+			./main 2022 "$month" "$date" "$i" &> /tmp/libradtran-interface.log
+		done
+	done
+done
+
+mv configs/pho30000 config.conf
+
 for month in 6; do
 # month=6
 	for date in 1 21; do	
@@ -15,8 +31,43 @@ for month in 6; do
 		done
 	done
 done
+mv configs/pho300000 config.conf
 
-mv configs/albedo0.9.conf config.conf
+for month in 6; do
+# month=6
+	for date in 1 21; do	
+#		for i in 5 10 15 20 25 30 35 40 45 50 65 70 75 80 85; do
+		for i in 5 15 35 45 75 85; do
+	 		echo "Progress: $month $date, $i"
+			./main 2022 "$month" "$date" "$i" &> /tmp/libradtran-interface.log
+		done
+	done
+done
+mv configs/pho6000 config.conf
+
+for month in 6; do
+# month=6
+	for date in 1 21; do	
+#		for i in 5 10 15 20 25 30 35 40 45 50 65 70 75 80 85; do
+		for i in 5 15 35 45 75 85; do
+	 		echo "Progress: $month $date, $i"
+			./main 2022 "$month" "$date" "$i" &> /tmp/libradtran-interface.log
+		done
+	done
+done
+mv configs/pho60000 config.conf
+
+for month in 6; do
+# month=6
+	for date in 1 21; do	
+#		for i in 5 10 15 20 25 30 35 40 45 50 65 70 75 80 85; do
+		for i in 5 15 35 45 75 85; do
+	 		echo "Progress: $month $date, $i"
+			./main 2022 "$month" "$date" "$i" &> /tmp/libradtran-interface.log
+		done
+	done
+done
+mv configs/pho600000 config.conf
 
 for month in 6; do
 # month=6
--- Working/fit/main.cpp
+++ Working/fit/main.cpp
@@ -2,17 +2,37 @@
 
 int main(int argc, char* argv[]){
 	if(argc != 2){
-		std::cerr << "Usage ./fit path" << std::endl;
-		return 1;
+		std::cerr << "Usage ./fit paths.txt" << std::endl;
 	}
-	std::string path = argv[1];
-	std::cerr << path << std::endl;
-	int Ndata;
-	double offset = fit::mean(path, 95.0, 100.0);
-	std::cout << offset << std::endl;
-	double* fitted = fit::fitting_result(path, 20.0, 60.0, offset, fit::LOG, &Ndata);
-	for(int i = 0; i<Ndata; i++){
-		std::cout << fitted[i] << " ";
+	std::string path;
+
+	std::string paths = argv[1];
+	std::ifstream ifs(paths);
+	std::string line;
+	
+	while(std::getline(ifs, line)){
+		if(line[0] != '#'){
+			std::istringstream iss_read(line);
+			std::string token;
+			while(iss_read >> token){
+				path = token;
+
+				/* -- */
+
+				std::cerr << path << std::endl;
+				int Ndata;
+				double offset = fit::mean(path, 95.0, 100.0);
+				std::cout << offset << std::endl;
+				double* fitted = fit::fitting_result(path, 20.0, 60.0, offset, fit::LOG, &Ndata);
+				for(int i = 0; i<Ndata; i++){
+					std::cout << fitted[i] << " ";
+				}
+				std::cout << std::endl;
+
+				/* ^^ */
+			}
+		}
 	}
-	std::cout << std::endl;
+
+	return 0;
 }
