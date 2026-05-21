c6cae36406351b33bb6ce2c503cf198dcac31d0f
sano <s2210729@gl.cc.uec.ac.jp>
2026-05-21 12:44:12 +0900

    typoを修正

--- src/main.cpp
+++ src/main.cpp
@@ -116,7 +116,7 @@ if(argc == 7){
 	std::string SURFACE_TYPE = getConfig(configs, "SURFACE_TYPE", "ABSORB");/* 表面反射のタイプを指定 */
 	double albedo = getConfig(configs, "albedo", 0.3);/* LAMBERT の反射率 */
 	double brdf_cam_u10 = getConfig(configs, "brdf_cam_u10", 15.0);/* BRDF_CAM の風速 */
-	int brdf_rpv_type = getConfig(configs, "brdf_rpv_type", 0);/* BRDF_CAM の風速 */
+	int brdf_rpv_type = getConfig(configs, "brdf_rpv_type", 0);/* BRDF(RPV)_IGBPのタイプ */
 	std::string additional_option = getConfig(configs, "additional_option", "");/* libRadtranの標準入力に追加で書き込む文字列 */
 	replaceAll(additional_option, "\\n", "\n");
 	int mc_photons = getConfig(configs, "mc_photons", 60000);/* MYSTICの回数 デフォルトは300000 */
